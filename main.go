8db5520dc7ffb7b4abc28b3f0661c9b1c89c428c
obzva <dngyng1000@gmail.com>
2025-02-28 23:06:14 -0800

    [feat] use two servers

--- main.go
+++ main.go
@@ -1,41 +1,78 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
-	"os"
 )
 
+const keyServerAddr = "serverAddr"
+
 /*
-	Handler function for /
+Handler function for /
 */
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
+	ctx := r.Context()
+
+	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "This is my website!\n")
 }
 
 /*
-	Handler function for /hello
+Handler function for /hello
 */
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "Hello HTTP!\n")
+}
+
+func runServer(s *http.Server, sName string, canceler context.CancelFunc) {
+	err := s.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("%s closed\n", sName)
+	} else if err != nil {
+		fmt.Printf("error listening for %s: %s\n", err, sName)
+	}
+	canceler()
 }
 
 func main() {
 	// make own custom HTTP request mux(multiplexer)
-    mux := http.NewServeMux()
+	mux := http.NewServeMux()
 	// register handler functions for given patterns
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe("127.0.0.1:3333", mux)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
+	// setting context
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
+	// setting server 1
+	serverOne := &http.Server{
+		Addr:    "127.0.0.1:3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
 	}
+
+	// setting server 2
+	serverTwo := &http.Server{
+		Addr:    "127.0.0.1:4444",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go runServer(serverOne, "server one", cancelCtx)
+	go runServer(serverTwo, "server two", cancelCtx)
+
+	<-ctx.Done()
 }
